b302a239bd69001b845cb2645407d0a0cd58a67a
RexShack <rexshackuk@gmail.com>
2023-01-19 06:51:36 +0000

    tidy-up

--- locales/en.lua
+++ locales/en.lua
@@ -9,16 +9,16 @@ local Translations = {
         weapon_reloaded = 'Weapon Reloaded',
     },
     primary = {
-		    var = 'text goes here',
+        var = 'text goes here',
     },
     menu = {
-		    var = 'text goes here',
+        var = 'text goes here',
     },
     commands = {
-		    var = 'text goes here',
+        var = 'text goes here',
     },
     progressbar = {
-		    var = 'text goes here',
+        var = 'text goes here',
     },
 }
 
