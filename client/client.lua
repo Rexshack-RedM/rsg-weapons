3995b4166222fe3dd89b9f1e5c595ecd30352181
Dipankar Das <67905221+ProtoGhost@users.noreply.github.com>
2023-09-02 15:50:00 +0530

    fix ammo respawn issue on revive

--- client/client.lua
+++ client/client.lua
@@ -5,9 +5,7 @@ local UsedWeapons = {}
 local EquippedWeapons = {}
 
 exports('EquippedWeapons', function()
-    if EquippedWeapons ~= nil then
-        return EquippedWeapons
-    end
+    return EquippedWeapons
 end)
 
 -- Models Loader
@@ -26,16 +24,10 @@ local LoadModel = function(model)
 end
 
 local getGuidFromItemId = function(inventoryId, itemData, category, slotId)
-    local outItem = DataView.ArrayBuffer(8 * 13)
-
-    if not itemData then
-        itemData = 0
-    end
-
-    local success = Citizen.InvokeNative(0x886DFD3E185C8A89, inventoryId, itemData, category, slotId, outItem:Buffer())
-
-    if success then
-        return outItem:Buffer()
+    local outItem = Citizen.InvokeNative(0x886DFD3E185C8A89, inventoryId, itemData or 0, category, slotId)
+    
+    if outItem then
+        return outItem
     end
 
     return nil
@@ -63,16 +55,14 @@ local addWardrobeInventoryItem = function(itemName, slotHash)
         return false
     end
 
-    local itemData = DataView.ArrayBuffer(8 * 13)
-    local isAdded = Citizen.InvokeNative(0xCB5D11F9508A928D, inventoryId, itemData:Buffer(), wardrobeItem, itemHash, slotHash, 1, addReason)
+    local itemData = Citizen.InvokeNative(0xCB5D11F9508A928D, inventoryId, nil, wardrobeItem, itemHash, slotHash, 1, addReason)
 
-    if not isAdded then
-        return false
+    if itemData then
+        local equipped = Citizen.InvokeNative(0x734311E2852760D0, inventoryId, itemData, true)
+        return equipped
     end
 
-    local equipped = Citizen.InvokeNative(0x734311E2852760D0, inventoryId, itemData:Buffer(), true)
-
-    return equipped
+    return false
 end
 
 local GiveWeaponComponentToEntity = function(entity, componentHash, weaponHash, p3)
@@ -96,20 +86,11 @@ RegisterNetEvent('rsg-weapons:client:UseWeapon', function(weaponData, shootbool)
     local hash = GetHashKey(weaponData.name)
     local wepSerial = tostring(weaponData.info.serie)
 
-    for i = 1, #EquippedWeapons do
-        local usedHash = EquippedWeapons[i]
+    local isThrowable = string.find(weaponName, 'thrown') ~= nil
 
-        if hash == usedHash then
-            alreadyUsed = true
-        end
-    end
-
-    EquippedWeapons[#EquippedWeapons + 1] = hash
-
-    if not alreadyUsed and not UsedWeapons[tonumber(hash)] then
-
-        if string.find(weaponName, 'thrown') == false then
-            UsedWeapons[tonumber(hash)] = {
+    if not alreadyUsed and not UsedWeapons[hash] then
+        if not isThrowable then
+            UsedWeapons[hash] = {
                 name = weaponData.name,
                 WeaponHash = hash,
                 data = weaponData,
@@ -131,9 +112,7 @@ RegisterNetEvent('rsg-weapons:client:UseWeapon', function(weaponData, shootbool)
                 end
             end
             Citizen.InvokeNative(0x5E3BDDBCB83F3D84, ped, hash, 0, false, true)
-
-        --check throwables weapons
-        elseif string.find(weaponName, 'thrown') then
+        elseif isThrowable then
             GiveWeaponToPed_2(ped, hash, 0, false, true, 0, false, 0.5, 1.0, 752097756, false, 0.0, false)
             TriggerServerEvent('rsg-weapons:server:removeWeaponItem', weaponName, 1)
         else
@@ -143,10 +122,10 @@ RegisterNetEvent('rsg-weapons:client:UseWeapon', function(weaponData, shootbool)
             end
             Citizen.InvokeNative(0x5E3BDDBCB83F3D84, ped, hash, 0, false, true)
         end
+
         if weaponName == 'weapon_bow' or weaponName == 'weapon_bow_improved' then
             Citizen.InvokeNative(0x14E56BC5B5DB6A19, ped, hash, weaponData.info.ammo)
-
-        elseif  string.find(weaponName, 'thrown') then
+        elseif isThrowable then
             local _ammoType = Config.AmmoTypes[weaponName]
             Citizen.InvokeNative(0x106A811C6D3035F3, ped, _ammoType, Config.AmountThrowablesAmmo, 752097756)
         else
@@ -165,24 +144,15 @@ RegisterNetEvent('rsg-weapons:client:UseWeapon', function(weaponData, shootbool)
 
         TriggerServerEvent('rsg-weapons:server:LoadComponents', wepSerial, hash)
 
-        SetCurrentPedWeapon(ped,hash,true)
+        SetCurrentPedWeapon(ped, hash, true)
 
     else
         local ammo = GetAmmoInPedWeapon(PlayerPedId(), hash)
-        local ammobool, ammoclip = GetAmmoInClip(PlayerPedId(),hash)
+        local ammobool, ammoclip = GetAmmoInClip(PlayerPedId(), hash)
         TriggerServerEvent('rsg-weapons:server:SaveAmmo', weaponData.info.serie, ammo, ammoclip)
         print('removing weapon ')
-        RemoveWeaponFromPed(ped,hash)
-        UsedWeapons[tonumber(hash)] = nil
-
-        for i = 1, #EquippedWeapons do
-            local usedHash = EquippedWeapons[i]
-
-            if hash == usedHash then
-                EquippedWeapons[i] = nil
-                alreadyUsed = false
-            end
-        end
+        RemoveWeaponFromPed(ped, hash)
+        UsedWeapons[hash] = nil
     end
     currentSerial = weaponData.info.serie
 end)
@@ -210,7 +180,6 @@ RegisterNetEvent('rsg-weapons:client:AddAmmo', function(ammotype, amount, ammo)
     local ped = PlayerPedId()
     local weapon = Citizen.InvokeNative(0x8425C5F057012DAB, ped)
     local weapongroup = GetWeapontypeGroup(weapon)
-    local currentSerial = currentSerial
     local max_ammo = 0
     local ammo_type = ''
     local valid_ammo = false
@@ -301,7 +270,7 @@ CreateThread(function()
         local ped = PlayerPedId()
         local heldWeapon = Citizen.InvokeNative(0x8425C5F057012DAB, ped)
         local getammo = GetAmmoInPedWeapon(ped, heldWeapon)
-        local _,getammoclip = GetAmmoInClip(ped, heldWeapon)
+        local _, getammoclip = GetAmmoInClip(ped, heldWeapon)
         if currentSerial ~= nil and heldWeapon ~= -1569615261 then
             TriggerServerEvent('rsg-weapons:server:SaveAmmo', currentSerial, tonumber(getammo), tonumber(getammoclip))
         end
@@ -311,8 +280,8 @@ end)
 Citizen.CreateThread(function()
     while true do
         Wait(1)
-        SetPlayerWeaponDamageModifier(PlayerId(),Config.WeaponDmg)
-        SetPlayerMeleeWeaponDamageModifier(PlayerId(),Config.MeleeDmg)
+        SetPlayerWeaponDamageModifier(PlayerId(), Config.WeaponDmg)
+        SetPlayerMeleeWeaponDamageModifier(PlayerId(), Config.MeleeDmg)
         if IsPlayerFreeAiming(PlayerId()) then
             DisableControlAction(0, 0x8FFC75D6, true)
         end
