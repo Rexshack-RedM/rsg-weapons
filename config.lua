0589585a1eafb31461d8424c371d6fb7a94d8cc4
Artmines <96462463+Artmines@users.noreply.github.com>
2024-02-16 18:18:58 -0700

    Update config.lua Easy Reload Toggle

--- config.lua
+++ config.lua
@@ -5,7 +5,8 @@ Config.Debug = false
 -- settings
 --Config.UpdateAmmo = 10000 -- amount of time before saving player ammo in miliseconds
 Config.RepairTime = 30000
-Config.AmmoReloadKeybind = 'J'
+Config.EasyReload = true        -- Set to false if you want to disable easy reload
+Config.AmmoReloadKeybind = 'R'
 -- weapon degradation
 Config.DegradeRate = 0.01
 
