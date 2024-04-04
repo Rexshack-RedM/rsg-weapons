ded51116e89667a1b5cabf10b0dfbf1224f860e8
TehGuardian <coreman89@gmail.com>
2024-04-04 19:34:20 -0400

    Version Check

--- server/server.lua
+++ server/server.lua
@@ -1,9 +1,31 @@
-local RSGCore = exports['rsg-core']:GetCoreObject()
-
 -----------------------------------------------------------------------
 -- version checker
 -----------------------------------------------------------------------
-lib.versionCheck('Rexshack-RedM/rsg-weapons')
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-weapons/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then 
+            versionCheckPrint('error', 'Currently unable to run a version check.')
+            return 
+        end
+
+        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
+        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
+
+        if text == currentVersion then
+            versionCheckPrint('success', 'You are running the latest version.')
+        else
+            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
+        end
+    end)
+end
 
 ------------------------------------------
 -- use weapon repair kit
@@ -125,3 +147,8 @@ AddEventHandler('rsg-weapons:server:removeitem', function(item, amount)
     Player.Functions.RemoveItem(item, amount)
     TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items[item], "remove")
 end)
+
+--------------------------------------------------------------------------------------------------
+-- start version check
+--------------------------------------------------------------------------------------------------
+CheckVersion()
