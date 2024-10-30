18bda9ae858756436615e0c8cdb59ebebea6ae30
RexshackGaming <rexshackuk@gmail.com>
2024-10-30 19:58:19 +0000

    version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 description 'rsg-weapons'
-version '2.0.0'
+version '2.0.1'
 
 shared_scripts {
     '@ox_lib/init.lua',
