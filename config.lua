1d155e03a3d9bb01f96ea362b0ba556af7485768
DAN <87706283+ItsMeD4N@users.noreply.github.com>
2025-10-28 12:12:34 +0700

    feat(weapon): Add damage multiplier for each weapon and config for disable critical shot

--- config.lua
+++ config.lua
@@ -12,8 +12,74 @@ Config.DegradeRate = 0.01
 Config.UpdateDegrade = 5000
 
 --- Damage Weapon
-Config.WeaponDmg = 0.65
-Config.MeleeDmg = 1.0
+Config.DisableCriticalHits = false 
+Config.WeaponDamage = {
+    -- Melee
+    {Name = 'WEAPON_UNARMED', Damage = 1.0},
+    {Name = 'WEAPON_MELEE_CLEAVER', Damage = 1.0},
+    {Name = 'WEAPON_MELEE_HAMMER', Damage = 1.0},
+    {Name = 'WEAPON_MELEE_HATCHET', Damage = 1.0},
+    {Name = 'WEAPON_MELEE_HATCHET_HUNTER', Damage = 1.0},
+    {Name = 'WEAPON_MELEE_KNIFE', Damage = 1.0},
+    {Name = 'WEAPON_MELEE_KNIFE_HORROR', Damage = 1.0},
+    {Name = 'WEAPON_MELEE_KNIFE_JAWBONE', Damage = 1.0},
+    {Name = 'WEAPON_MELEE_KNIFE_RUSTIC', Damage = 1.0},
+    {Name = 'WEAPON_MELEE_KNIFE_TRADER', Damage = 1.0},
+    {Name = 'WEAPON_MELEE_MACHETE', Damage = 1.0},
+    {Name = 'WEAPON_MELEE_MACHETE_COLLECTOR', Damage = 1.0},
+    {Name = 'WEAPON_MELEE_MACHETE_HORROR', Damage = 1.0},
+
+    -- Revolver
+    {Name = 'WEAPON_REVOLVER_CATTLEMAN', Damage = 1.0},
+    {Name = 'WEAPON_REVOLVER_CATTLEMAN_MEXICAN', Damage = 1.0},
+    {Name = 'WEAPON_REVOLVER_DOUBLEACTION', Damage = 1.0},
+    {Name = 'WEAPON_REVOLVER_DOUBLEACTION_GAMBLER', Damage = 1.0},
+    {Name = 'WEAPON_REVOLVER_LEMAT', Damage = 1.0},
+    {Name = 'WEAPON_REVOLVER_NAVY', Damage = 1.0},
+    {Name = 'WEAPON_REVOLVER_NAVY_CROSSOVER', Damage = 1.0},
+    {Name = 'WEAPON_REVOLVER_SCHOFIELD', Damage = 1.0},
+
+    -- Pistols
+    {Name = 'WEAPON_PISTOL_M1899', Damage = 1.0},
+    {Name = 'WEAPON_PISTOL_MAUSER', Damage = 1.0},
+    {Name = 'WEAPON_PISTOL_SEMIAUTO', Damage = 1.0},
+    {Name = 'WEAPON_PISTOL_VOLCANIC', Damage = 1.0},
+
+    -- Snipers
+    {Name = 'WEAPON_SNIPERRIFLE_CARCANO', Damage = 1.0},
+    {Name = 'WEAPON_SNIPERRIFLE_ROLLINGBLOCK', Damage = 1.0},
+
+    -- Rifle
+    {Name = 'WEAPON_RIFLE_BOLTACTION', Damage = 1.0},
+    {Name = 'WEAPON_RIFLE_ELEPHANT', Damage = 1.0},
+    {Name = 'WEAPON_RIFLE_SPRINGFIELD', Damage = 1.0},
+    {Name = 'WEAPON_RIFLE_VARMINT', Damage = 1.0},
+
+    -- Repeater
+    {Name = 'WEAPON_REPEATER_CARBINE', Damage = 1.0},
+    {Name = 'WEAPON_REPEATER_EVANS', Damage = 1.0},
+    {Name = 'WEAPON_REPEATER_HENRY', Damage = 1.0},
+    {Name = 'WEAPON_REPEATER_WINCHESTER', Damage = 1.0},
+
+    -- Thrown
+    {Name = 'WEAPON_THROWN_DYNAMITE', Damage = 1.0},
+    {Name = 'WEAPON_THROWN_POISONBOTTLE', Damage = 1.0},
+    {Name = 'WEAPON_THROWN_THROWING_KNIVES', Damage = 1.0},
+    {Name = 'WEAPON_THROWN_TOMAHAWK', Damage = 1.0},
+    {Name = 'WEAPON_THROWN_TOMAHAWK_ANCIENT', Damage = 1.0},
+    --[[ {Name = 'WEAPON_THROWN_MOLOTOV', Damage = 1.0}, ]] -- Has no effect
+
+    -- Shotgun
+    {Name = 'WEAPON_SHOTGUN_DOUBLEBARREL', Damage = 1.0},
+    {Name = 'WEAPON_SHOTGUN_PUMP', Damage = 1.0},
+    {Name = 'WEAPON_SHOTGUN_REPEATING', Damage = 1.0},
+    {Name = 'WEAPON_SHOTGUN_SAWEDOFF', Damage = 1.0},
+    {Name = 'WEAPON_SHOTGUN_SEMIAUTO', Damage = 1.0},
+
+    -- Bows
+    {Name = 'WEAPON_BOW', Damage = 1.0},
+    {Name = 'WEAPON_BOW_IMPROVED', Damage = 1.0},
+}
 
 Config.ThrowableWeaponAmmoTypes = {
     ['weapon_thrown_throwing_knives'] = 'AMMO_THROWING_KNIVES',
